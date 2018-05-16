79a65b5ad1d3288d6b105abb3a294ed53600da6d
Abhishek Kedia <kedia.abhishek10@gmail.com>
2018-05-17 02:26:22 +0530

    Add stream procession utils

---
+++ client-react/src/utils/media-stream-utils.js
@@ -0,0 +1,30 @@
+const getAudioTrack = stream =>
+    stream.getAudioTracks()[0];
+
+const getVideoTrack = stream =>
+    stream.getVideoTracks()[0];
+
+export const hasAudio = stream => {
+    const audioTrack = getAudioTrack(stream);
+    return audioTrack && audioTrack.readyState === "live";
+}
+
+export const hasVideo = stream => {
+    const videoTrack = getVideoTrack(stream);
+    return videoTrack && videoTrack.readyState;
+}
+
+const removeAndStopTrack = (stream, track) => {
+    stream.removeTrack(track);
+    track.stop();
+}
+
+export const removeAndStopAudioTrack = stream => {
+    const audioTrack = getAudioTrack(stream);
+    audioTrack && removeAndStopTrack(stream, audioTrack);
+}
+
+export const removeAndStopVideoTrack = stream => {
+    const videoTrack = getVideoTrack(stream);
+    videoTrack && removeAndStopTrack(stream, videoTrack);
+}
