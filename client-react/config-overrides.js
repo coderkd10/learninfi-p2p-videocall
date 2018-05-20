331eba40de54b5e7469fe7f2e8b7b14fbea26812
Abhishek Kedia <kedia.abhishek10@gmail.com>
2018-05-20 14:23:19 +0530

    Add webpack bundle analyzer during build

--- client-react/config-overrides.js
+++ client-react/config-overrides.js
@@ -1,8 +1,15 @@
 const rewireMobX = require('react-app-rewire-mobx');
 const rewireCssModules = require('react-app-rewire-css-modules');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = function override(config, env) {
     config = rewireMobX(config, env);
     config = rewireCssModules(config, env);
+    if (env === 'production') {
+        config.plugins.push(new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            openAnalyzer: false
+        }));
+    }
     return config;
 }
