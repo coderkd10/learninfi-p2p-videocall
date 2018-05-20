dc7473cf89f7406fd7af5ea31ab0a3b499330481
Abhishek Kedia <kedia.abhishek10@gmail.com>
2018-05-21 02:18:47 +0530

    make the language in the comment clearer and correct typos

--- client-react/src/shared/cancelifyPromise.js
+++ client-react/src/shared/cancelifyPromise.js
@@ -1,5 +1,5 @@
 // todo: publish this as a standalone npm module
-// my simple implementation of a cacelable promise
+// my implementation of a cancelable promise
 
 export const CANCELED_PROMISE_EXCEPTION_NAME = "CanceledPromiseException";
 
@@ -10,10 +10,13 @@ const generateCanceledPromiseException = () => {
 }
 
 // this function takes a promise and makes it cancelable
-// i.e attaches a `cancel` method to it
-// if the cancel method is not called the returned promise
-// behaves like the original promise
-// but after the cancel 
+// i.e attaches a `cancel` method to the returned promise.
+// until the cancel method is not called the returned promise
+// behaves just like the original promise (its then / catch
+// handlers are triggered when the original's are triggered
+// with the same inputs) but as soon as the cancel function is
+// called the returned promise is rejected with an exception
+// (which can be identified by its name -> "CanceledPromiseException")
 const cancelifyPromise = origPromise => {
     let cbs = null; // reference to new promise's callbacks
     const newPromise = new Promise((resolve, reject) => {
