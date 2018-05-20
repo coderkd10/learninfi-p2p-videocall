8bbd71a2bcb3023a4f457ad8b2dc737cc921800c
Abhishek Kedia <kedia.abhishek10@gmail.com>
2018-05-20 21:54:54 +0530

    Complete app's presentational component

--- client-react/src/presentational/App.js
+++ client-react/src/presentational/App.js
@@ -2,21 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PeersArea from './PeersArea';
 import ToolsContainer from './ToolsContainer';
+import Webcam from '../container/Webcam';
+import VideoContainer from '../container/VideoContainer';
 import styles from './App.module.css';
 
 const BORDER_SIZE = 1;
 const TOOLS_CONTAINER_MAX_HEIGHT = 39;
+const VIDEO_PADDING = 5;
 
 const App = ({
     width,
     height,
-    style
+    style,
+    captureAudio,
+    captureVideo,
 }) => {
     const innerWidth = width - 2*BORDER_SIZE;
     const innerHeight = height - 2*BORDER_SIZE;
     const peersAreaHeight = 0.3*innerHeight;
     const toolsContainerHeight = Math.min(TOOLS_CONTAINER_MAX_HEIGHT, 0.15*innerHeight);
-    
+    const videoContainerHeight = innerHeight - (peersAreaHeight + toolsContainerHeight + VIDEO_PADDING);
+    const videoContainerWidth = innerWidth - 2*VIDEO_PADDING;
+
     return (
         <div className={styles.container} style={{
             width,
@@ -28,6 +35,31 @@ const App = ({
                 width={innerWidth}
                 height={peersAreaHeight}
             />
+            <div style={{
+                paddingTop: VIDEO_PADDING,
+                paddingLeft: VIDEO_PADDING,
+                paddingRight: VIDEO_PADDING,
+            }}>
+                <Webcam
+                    audio={captureAudio}
+                    video={captureVideo}
+                >
+                {
+                    ({
+                    err,
+                    showLoading,
+                    stream  
+                    }) => (
+                        <VideoContainer
+                            width={videoContainerWidth}
+                            height={videoContainerHeight}
+                            showLoading={showLoading}
+                            showError={Boolean(err)}
+                            stream={stream}
+                        />)
+                }
+                </Webcam>
+            </div>
             <ToolsContainer
                 width={innerWidth}
                 height={toolsContainerHeight}
@@ -41,6 +73,8 @@ App.propTypes = {
     width: PropTypes.number.isRequired,
     height: PropTypes.number.isRequired,
     style: PropTypes.object,
+    captureAudio: PropTypes.bool.isRequired,
+    captureVideo: PropTypes.bool.isRequired,
 };
 
 App.defaultProps = {
