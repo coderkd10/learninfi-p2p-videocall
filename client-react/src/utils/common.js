63d63432677aeb8e2787202d4a97af8e2c2300cb
Abhishek Kedia <kedia.abhishek10@gmail.com>
2018-05-28 05:25:59 +0530

    Move utils to react app src directory otherwise it was failing to compile
    Since we were using es6 arrow function minification was failing

---
+++ client-react/src/utils/common.js
@@ -0,0 +1,14 @@
+const removeArrayIndex = (array, i) => {
+    if (i < 0 || i >= array.length) {
+        return array;
+    }
+    return [...array.slice(0, i), ...array.slice(i+1)]    
+}
+
+const removeInArray = (array, element) =>
+    removeArrayIndex(array, array.indexOf(element));
+
+
+module.exports = {
+    removeInArray,
+};
--- common/utils.js
+++ common/utils.js
@@ -1,14 +1,1 @@
-const removeArrayIndex = (array, i) => {
-    if (i < 0 || i >= array.length) {
-        return array;
-    }
-    return [...array.slice(0, i), ...array.slice(i+1)]    
-}
-
-const removeInArray = (array, element) =>
-    removeArrayIndex(array, array.indexOf(element));
-
-
-module.exports = {
-    removeInArray
-};
+../client-react/src/utils/common.js
